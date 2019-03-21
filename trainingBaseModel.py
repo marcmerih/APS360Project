fce7bc8beac43a6ee16dcf3fcb51cec2dc52a6e2
itazap <31893021+itazap@users.noreply.github.com>
2019-03-21 12:33:32 -0400

    Add files via upload

--- trainingBaseModel.py
+++ trainingBaseModel.py
@@ -1,27 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 13:04:14 2019
+
+@author: chris
+"""
 import definitions as d
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 
-#---------------------Data Visualization Examples----------------------------------
-    
 
-#trainTemp,valTemp,testTemp = d.get_data_loader(1)
-#k = 0
-#for images, labels in trainTemp:
-#    # since batch_size = 1, there is only 1 image in `images`
-#    image = images[0]
-#    # place the colour channel at the end, instead of at the beginning
-#    img = np.transpose(image, [1,2,0])
-#    # normalize pixel intensity values to [0, 1]
-#    img = img / 2 + 0.5
-#    plt.subplot(3, 5, k+1)
-#    plt.axis('off')
-#    plt.imshow(img)
-#
-#    k += 1
-#    if k > 14:
-#        break
-    
-modelBase = d.BaseModel(input_size = 400)
-iterations,train_acc, val_acc = d.train(mdl = modelBase,epochs = 5)
+modelBase = d.BaseModel(input_size = 600)
+iterations,train_acc, val_acc = d.train(mdl = modelBase,epochs = 10,batch_size = 32)
 d.plot(iterations,train_acc, val_acc)
+
