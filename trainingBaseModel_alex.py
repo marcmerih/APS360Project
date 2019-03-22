55b3cf0443676590d3d3bc59300c560e40220d14
Merih <31850729+merihatasoy@users.noreply.github.com>
2019-03-21 20:44:40 -0400

    Add files via upload

---
+++ trainingBaseModel_alex.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thur Mar 21 08:43:49 2019
+
+@author: marc
+"""
+import definitions_v3_alex as da
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+modelBase = da.AlexNet()
+iterations,train_acc = da.alextrain(modelBase,batch_size = 32, epochs = 10,lr = 0.0001)
+da.plot(iterations,train_acc)
