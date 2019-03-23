eaad8b35a3e736eac69111ece9a25b2070cb9183
Merih <merih.atasoy@gmail.com>
2019-03-23 13:59:21 -0400

    vgg models

---
+++ trainingBaseModel_vgg.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 13:04:14 2019
+
+@author: chris
+"""
+import definitions_v4_vgg as d
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+model = d.VGGModel(input_size = 600)
+iterations,train_acc, val_acc = d.train(mdl = modelBase,epochs = 10,batch_size = 32)
+d.plot(iterations,train_acc, val_acc)
