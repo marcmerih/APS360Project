e182b108295c2da10adc7363f9959bdf3c0d227c
Merih <merih.atasoy@gmail.com>
2019-03-23 14:35:27 -0400

    pictures

--- trainingBaseModel_vgg.py
+++ trainingBaseModel_vgg.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 12 13:04:14 2019
@@ -10,6 +11,23 @@ import numpy as np
 import pickle
 
 
-model = d.VGGModel(input_size = 600)
+model = d.Model(input_size = 600)
 iterations,train_acc, val_acc = d.train(mdl = modelBase,epochs = 10,batch_size = 32)
 d.plot(iterations,train_acc, val_acc)
+=======
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 13:04:14 2019
+
+@author: chris
+"""
+import definitions_v4_vgg as d
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+model = d.Model(input_size = 600)
+iterations,train_acc, val_acc = d.train(mdl = modelBase,epochs = 10,batch_size = 32)
+d.plot(iterations,train_acc, val_acc)
+>>>>>>> a23278a45f045abb6a38b1d574e81763a2298e25
