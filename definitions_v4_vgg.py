3d814c33d3591f229254aa3c20208330be594e4d
Merih <31850729+merihatasoy@users.noreply.github.com>
2019-03-23 16:49:33 -0400

    Add files via upload

--- definitions_v4_vgg.py
+++ definitions_v4_vgg.py
@@ -62,7 +62,7 @@ class Model(nn.Module):
         self.model_vgg16_conv.summary()
         self.output_vgg16_conv = self.model_vgg16_conv(self.input)
         self.pool = nn.MaxPool2d(2, 2)
-        self.x1 = Flatten(name='flatten')(output_vgg16_conv)
+        self.x1 = Flatten(name='flatten')(self.output_vgg16_conv)
         self.x2 = Dense(4096, activation='relu', name='x2')(self.x1)
         self.x3 = Dense(4096, activation='relu', name='x3')(self.x2)
         self.x4 = Dense(2, activation='softmax', name='predictions')(self.x3)
