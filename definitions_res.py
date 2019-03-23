6d02d7fa6e77e5792894b66e38d67072ed46ce9f
Palumbo93 <chrispalumbo1998@hotmail.com>
2019-03-23 15:31:40 -0400

    Update definitions_res.py

--- definitions_res.py
+++ definitions_res.py
@@ -29,7 +29,7 @@ from resnet import *
 
 import torchvision.models as models
 resnet18 = resnet18(pretrained=True)
-resnet18 = resnet18.cuda()
+
 
 #--------------------Data Loading and Splitting ---------------------------------
 def get_data_loader(batch_size):
