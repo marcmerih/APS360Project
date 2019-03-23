f9262ba9da4b445f3cd45ab589a1cdb816517a74
Palumbo93 <chrispalumbo1998@hotmail.com>
2019-03-23 14:02:18 -0400

    Try 3

---
+++ trainingResNet.py
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+import definitions_res as d
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+modelres = d.ResNet()
+iterations,train_acc = d.train(mdl = modelBase,epochs = 20,batch_size = 16) #,val_acc
+#d.plot(iterations,train_acc, val_acc)
+=======
+import definitions_res as d
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+
+modelres = d.ResNet()
+iterations,train_acc = d.train(mdl = modelBase,epochs = 20,batch_size = 16) #,val_acc
+#d.plot(iterations,train_acc, val_acc)
+>>>>>>> 45ccdd150aae0148d15635a09297f89f2c473889
