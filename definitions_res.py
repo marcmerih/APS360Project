57f53346c8f9bef630f4ec8ddcfaa0175b33bd7f
Palumbo93 <chrispalumbo1998@hotmail.com>
2019-03-24 22:48:22 -0400

    Update definitions_res.py

--- definitions_res.py
+++ definitions_res.py
@@ -28,7 +28,7 @@ from resnet import *
 
 
 import torchvision.models as models
-resnet18 = resnet18(pretrained=True).cuda()
+resnet18 = resnet18(pretrained=True)
 
 
 #--------------------Data Loading and Splitting ---------------------------------
@@ -81,13 +81,22 @@ class ResNet(nn.Module):
     def __init__(self,):
         super(ResNet, self).__init__()
         self.name = "ResNet"
-        self.fc1 = nn.Linear( 86528,100)
-        self.fc2 = nn.Linear(100, 2)
+        self.fc1 = nn.Linear( 86528,300)
+        self.fc2 = nn.Linear( 300,100)
+        self.fc3 = nn.Linear( 100,32)
+        self.fc4 = nn.Linear(32, 2)
 
     def forward(self, x):
-        x = x.view(-1, 86528)
+        #print(x.size())
+        #x = x.view(-1, 86528)
+        #print(x.size())
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        #print(x.size())
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        #x = x.squeeze(1)
+        #print(x.size(),"\n\n\n")
         return x
 
 
@@ -95,43 +104,85 @@ class ResNet(nn.Module):
 #-------------------Train Loop (Ft. Get Accuracy & Plotting)----------------------------------------
 
 
+#
+#def get_accuracy(model,set_,batch_size):
+#   # label_ = [0]*(batch_size)
+#   # label_.extend([1]*(batch_size))
+#
+#    label_ = [0]*(batch_size*2)
+#    for i in range(1,batch_size*2,2):
+#        label_[i] = 1
+#        
+#    label = torch.tensor(label_)#.type(torch.FloatTensor)
+#   # label = torch.tensor(label_).cuda()
+#
+#   # model = model.cuda()
+#    trainSet_,valSet_ = get_RN_data_loader(batch_size)
+#    
+#    if set_ == "train":
+#        data_ = trainSet_
+#    elif set_ == "val":
+#        data_ = valSet_
+#
+#
+#    correct = 0
+#    total = 0
+#
+#    for res, batch in data_:
+##        res = res.view(-1, 86528)
+##        prob = torch.sigmoid(model(res))
+##        pred = (prob > 0.5).type(torch.FloatTensor)
+##        correct = (pred == label).type(torch.FloatTensor)
+##        break
+##    return float(torch.mean(correct))
+#        
+#        if len(batch)==batch_size:
+#            res = res.view(-1, 86528)
+#            output = model(res)
+#
+#            pred = output.max(1, keepdim=True)[1]
+#                # get the index of the max log-probability
+#            correct += pred.eq(label.view_as(pred)).sum().item() #compute how many predictions were correct
+#            total += res.shape[0]*res.shape[1]
+#            #print(correct,res.shape[0]*res.shape[1])#get the total ammount of predictions
+#    #print(pred)
+#    #print("\n\n\n\n\n",label)      
+#    return correct / total
 
 def get_accuracy(model,set_,batch_size):
-    label_ = [0]*(batch_size)
-    label_.extend([1]*(batch_size))
-
-
-    label = torch.tensor(label_).cuda()
-
-    model = model.cuda()
-    trainSet_,valSet_ = get_data_loader(batch_size)
+    label_ = [0]*(batch_size*2)
+    for i in range(1,batch_size*2,2):
+        label_[i] = 1
+        
+        
+    label = torch.tensor(label_)
     
+    trainSet_,valSet_ = get_RN_data_loader(batch_size)
     if set_ == "train":
         data_ = trainSet_
     elif set_ == "val":
         data_ = valSet_
-
-
+    
+    
     correct = 0
     total = 0
-    for img, batch in data_:
-        if len(batch)==batch_size:
-            
-            b = torch.split(img,600,dim=3)
-            img = torch.cat(b, 0).cuda()
-    
-            res = resnet18(img)
-            output = model(res)
-            pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-            correct += pred.eq(label.view_as(pred)).sum().item() #compute how many predictions were correct
-            total += img.shape[0] #get the total ammount of predictions
-            break
-    
+    for res, batch in data_:
+     #   b = torch.split(img,600,dim=3) 
+      #  img = torch.cat(b, 0)
+        
+        res = res.view(-1, 86528)
+        output = model(res) 
+        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(label.view_as(pred)).sum().item() #compute how many predictions were correct
+        total += res.shape[0] #get the total ammount of predictions
+        break
+        
     return correct / total
+#
+from sklearn.utils import shuffle
 
-
-
-def train(mdl,epochs= 20,batch_size = 16,learning_rate =0.01):
+def train(mdl,epochs= 20,batch_size = 32,learning_rate =0.001):
+    #criterion = nn.BCEWithLogitsLoss()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(mdl.parameters(), lr=learning_rate, momentum=0.9)
     #trainSet,valSet = get_data_loader(batch_size)
@@ -139,12 +190,16 @@ def train(mdl,epochs= 20,batch_size = 16,learning_rate =0.01):
     train_acc, val_acc = [], []
     n = 0 # the number of iterations
 
-    label_ = [0]*(batch_size)
-    label_.extend([1]*(batch_size))
+#    label_ = [0]*(batch_size)
+#    label_.extend([1]*(batch_size))
+#    
+    label_ = [0]*(batch_size*2)
+    for i in range(1,batch_size*2,2):
+        label_[i] = 1
 
 
-    label = torch.tensor(label_).cuda()
-    mdl = mdl.cuda()
+    label = torch.tensor(label_)#.type(torch.FloatTensor)
+    #mdl = mdl.cuda()
     print("--------------Starting--------------")
 
 
@@ -157,19 +212,30 @@ def train(mdl,epochs= 20,batch_size = 16,learning_rate =0.01):
 
 
         for res,batch in iter(trainSet):
-
+            
             if len(batch)==batch_size:
+
+                res = res.view(-1, 86528)
                 
-               # b = torch.split(img,600,dim=3)
-
-
-              #  res = torch.cat(b, 0).cuda()
-
+                res = res.cpu().detach().numpy()
+                label = label.cpu().detach().numpy()
+                #print(res,label)
+                res, label = shuffle(res, label, random_state=0)
+                #print(res,label)
+                res = torch.tensor(res)
+                label = torch.tensor(label)
+                
+                #print(res.size(),batch.size())
+                #x = torch.squeeze(res,1)
+                #print(res)
+               
+                #res = torch.cat(b,0)
+                #print(x.shape)
                 #res = resnet18(img)
                 
                 out = mdl(res)
     
-    
+                #print(out.size())
                 loss = criterion(out, label)
                 loss.backward()
     
@@ -178,9 +244,9 @@ def train(mdl,epochs= 20,batch_size = 16,learning_rate =0.01):
                 #print("Iteration Done")
             
         # Calculate the statistics
-        train_acc.append(get_accuracy(mdl,"train",batch_size = 150))
+        train_acc.append(get_accuracy(mdl,"train",batch_size = 155))
     
-        val_acc.append(get_accuracy(mdl,"val",batch_size = 150))  # compute validation accuracy
+        val_acc.append(get_accuracy(mdl,"val",batch_size = 155))  # compute validation accuracy
         n += 1
     
     
@@ -212,46 +278,49 @@ def plot(iterations,train_acc, val_acc):
     print("Final Validation Accuracy: {}".format(val_acc[-1]))
 
 
-RtrainSet,RvalSet = get_data_loader(1)
+#RtrainSet,RvalSet = get_data_loader(1)
 
 def RNFeatures(dataSet,type_):
-    data = dataSet
+    data_ = dataSet
     i = 0
     j= 0
 
-    for img, label in data:
+    for img, label in data_:
         b = torch.split(img,600,dim=3)
 
-        img = torch.cat(b, 0).cuda()
-        
+        img = torch.cat(b, 0)
+        print(img.size())
         output = resnet18(img)
         
 
 
         if type_ == 'train':
-            tensor_path = "tensor_set{1}_number{2}".format(type_,i)
+            tensor_path = "tensor_set{0}_number{1}".format(type_,i)
 
             torch.save(output,r'C:/Users/chris/OneDrive/Documents/GitHub/APS360Project/RtrainData/'+tensor_path)
             i+=1
         elif type_ == 'val':
-            tensor_path = "tensor_set{1}_number{2}".format(type_,j)
+            tensor_path = "tensor_set{0}_number{1}".format(type_,j)
 
-            torch.save(output,r'C:/Users/chris/OneDrive/Documents/GitHub/APS360Project/RtrainData/'+tensor_path)
+            torch.save(output,r'C:/Users/chris/OneDrive/Documents/GitHub/APS360Project/RvalData/'+tensor_path)
             j+=1
-
-RNFeatures(RtrainSet,'train')
-RNFeatures(RvalSet,'val')
+#print("Train")
+#RNFeatures(RtrainSet,'train')
+#print("Validation")
+#RNFeatures(RvalSet,'val')
 
 def get_RN_data_loader(batch_size):
 
-    train_path = r'RtrainData'
-    val_path = r'RvalData'
+    #train_path = r'RtrainData'
+    #val_path = r'RvalData'
+    train_path = r'C:/Users/chris/OneDrive/Documents/GitHub/APS360Project/RtrainData'
+    val_path = r'C:/Users/chris/OneDrive/Documents/GitHub/APS360Project/RvalData'
 
 
     trainSet = torchvision.datasets.DatasetFolder(root=train_path,loader = torch.load,extensions = list(['']))
     train_data_loader = torch.utils.data.DataLoader(trainSet, batch_size=batch_size, shuffle=True)
 
-    valSet = torchvision.datasets.DatasetFolder(root=train_path,loader = torch.load,extensions = list(['']))
+    valSet = torchvision.datasets.DatasetFolder(root=val_path,loader = torch.load,extensions = list(['']))
     val_data_loader = torch.utils.data.DataLoader(valSet, batch_size=batch_size, shuffle=True)
 
     #testSet = torchvision.datasets.DatasetFolder(root=test_path,loader = torch.load,extensions = list(['']))
@@ -259,4 +328,4 @@ def get_RN_data_loader(batch_size):
    
     return train_data_loader ,val_data_loader
 
-RtrainSet,RvalSet = get_Alex_data_loader(16)
+RtrainSet,RvalSet = get_RN_data_loader(16)
