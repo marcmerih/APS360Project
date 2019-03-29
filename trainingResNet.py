f668783227caf8e0496a7bb824fbd4a1be3c4924
itazap <31893021+itazap@users.noreply.github.com>
2019-03-29 18:41:04 -0400

    Update trainingResNet.py

--- trainingResNet.py
+++ trainingResNet.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 
 
-modelres5 = d.ResNet5()
+modelres5 = d.ResNet5(dropout=0)
 iterations,train_acc,val_acc = d.train(mdl = modelres5,epochs = 30,batch_size = 32, learning_rate =0.001, weight_decay =0.001) #,val_acc
 d.plot(iterations,train_acc, val_acc)
 
