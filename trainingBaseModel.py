c778182588424939efd05a18ce01262016b2d61c
Merih <merih.atasoy@gmail.com>
2019-03-30 13:36:34 -0400

    resolve

--- trainingBaseModel.py
+++ trainingBaseModel.py
@@ -4,13 +4,13 @@ Created on Tue Mar 12 13:04:14 2019
 
 @author: chris
 """
-import definitions_v2 as d
+import definitions_v3 as d
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
 
 modelBase = d.BaseModel(input_size = 600)
-iterations,train_acc = d.train(mdl = modelBase,epochs = 40,batch_size = 16, learning_rate =0.001) #,val_acc
+iterations,train_acc = d.train(mdl = modelBase,epochs = 40,batch_size = 16, learning_rate =0.0003) #,val_acc
 #d.plot(iterations,train_acc, val_acc)
 
